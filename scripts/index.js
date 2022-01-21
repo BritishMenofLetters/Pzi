4feed228255d5328fcde8ebf9a275f7f3208d843
BritishMenofLetters <mstank00@fesb.hr>
2022-01-21 05:00:55 +0100

    Mobitel

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,14 @@ window.onscroll = () => {
         navbar.classList.remove('nav-active');
     }
 };
+
+
+let navbarmobile = document.querySelector('.navbar-mobile');
+
+window.onscroll = () => {
+    if (window.scrollY) {
+        navbarmobile.classList.add('navbar-mobile-active');
+    } else {
+        navbarmobile.classList.remove('navbar-mobile-active');
+    }
+};
